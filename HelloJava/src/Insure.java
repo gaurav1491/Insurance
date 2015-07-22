e362396dc0b898d345a25eee525a36abbaa00594
Gaurav Chaudhary <gauravchaudhary1491@gmail.com>
2015-07-22 10:44:14 +0530

    Initial Checkin of Insurer

---
+++ HelloJava/src/Insure.java
@@ -0,0 +1,11 @@
+
+public class Insure {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+               System.out.println("Hello we are in main" );
+               Insurer ins = new Insurer();
+               ins.getaccount_number();               
+	}
+
+}
---
+++ HelloJava/src/Insurer.java
@@ -0,0 +1,13 @@
+
+public class Insurer {
+
+	int  bankcode;
+	int account_number;
+	public void getbankcode(){
+		System.out.println("your ability is fruitful");
+	}
+	public void getaccount_number(){
+		System.out.println("your account number is bhepg4735l");
+	}
+	
+}
