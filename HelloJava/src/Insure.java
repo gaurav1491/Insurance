02ab283bd1bfef2c05e7a074ae33a7f1eea52197
sony <sony@sony-PC>
2015-07-22 11:13:03 +0530

    added first change

--- HelloJava/src/Insure.java
+++ HelloJava/src/Insure.java
@@ -3,7 +3,7 @@ public class Insure {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-               System.out.println("Hello we are in main" );
+               System.out.println("Hello we are in main java" );
                Insurer ins = new Insurer();
                ins.getaccount_number();               
 	}
